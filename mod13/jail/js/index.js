dd4c0d110f2cd4af3ebebd089b538cd2e0a7769d
SamFehl <sjfehl@pointpark.edu>
2024-04-15 12:06:55 -0400

    add fully working typewriter

--- mod13/jail/js/index.js
+++ mod13/jail/js/index.js
@@ -1,26 +1,11 @@
-//var text = ['a programmer','an accountant','a student', 'Samuel Fehl', 'a cheesemaker'];
-/*var i = 0;
-var txt = 'Lorem ipsum dummy text blabla.';
-var speed = 50;
-let delay = 25;
-var backspace = false;
-
-var outputElem = document.getElementById("type-writer-text");
-function typeWriter() {
-  outputElem.textContent = txt.substring(0, i);
-
-  if (i > txt.length + delay) backspace = true;
-  if (i == -1) backspace = false;
-  i = i + (backspace ? -1 : 1);
-  if(i != -1) setTimeout(typeWriter, speed);
-}*/
-
+//Typewriter Variables
 const texts = ["I am a programmer.", "I am an accountant.", "I am a student.", "I am Samuel Fehl."];
   let index = 0;
   let charIndex = 0;
   let isDeleting = false;
   let timeBeforeDeleting = 1000;
 
+  //Typewriter Function
   function type() {
     const currentText = texts[index];
     if (!isDeleting && charIndex < currentText.length) {
@@ -37,7 +22,7 @@ const texts = ["I am a programmer.", "I am an accountant.", "I am a student.", "
         index = (index + 1) % texts.length;
       }
     }
-    const speed = isDeleting ? 50 : 50; // Adjust speed here
+    const speed = isDeleting ? 50 : 50;
     const delay = isDeleting ? timeBeforeDeleting : speed;
     setTimeout(type, delay);
   }
